59a7d7ca3584b8ff1aeaee9848fa410bf1a415dd
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-04-12 17:37:49 -0600

    work on About Me styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
-				<div>
-					<Header />
-					<Switch>
-						<Route exact path="/" component={About} />
-						<Route exact path="/about" component={About} />
-						<Route exact path="/projects" component={Projects} />
-						<Route exact path="/contact" component={Contact} />
-						<Route exact path="*" component={About} />
-					</Switch>
-				</div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={About} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/projects" component={Projects} />
+					<Route exact path="/contact" component={Contact} />
+					<Route exact path="*" component={About} />
+				</Switch>
 			</Router>
 			<Footer />
 		</div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -11,29 +11,33 @@ function About() {
 							<hr />
 							<div className="row">
 								<div className="col-12">
-									<img
-										className="float-start me-3 p-3"
-										src="./assets/images/headshot.jpg"
-										id="headshot"
-										alt="Luke Oxner headshot"
-									/>
-									<h3 className="pt-4 px-3" id="hey">
-										Hey there, I'm Luke!
-									</h3>
+									<div className="row d-flex justify-content-center">
+										<div className="col-10 col-md-4">
+											<img
+												className="float-start me-3 p-3"
+												src="./assets/images/headshot.jpg"
+												id="headshot"
+												alt="Luke Oxner headshot"
+											/>
+										</div>
+										<div className="col-12 col-md-8">
+											<h3 className="pt-4 px-3 my-auto text-center" id="hey">
+												Hey there, I'm Luke!
+											</h3>
+										</div>
+									</div>
 									<p className="p-4">
 										I'm a full-stack developer based in the beautiful state of
 										Colorado, and a recent graduate of the University of
 										Denver's Full Stack Coding Bootcamp. I thrive when solving
 										complex problems and learning new things, and I am
-										passionate about functionality and making things work. I am
-										a strong communicator, fast learner, and an excellent team
-										player. Prior to entering the development space, I spent
-										four years as a professional racing driver, race team
-										manager, and marketing manager. I am excited to join a
-										software development team in which I can apply both my
-										technical coding skills, as well as the valuable teamwork
-										and leadership experience I gained during my previous career
-										in motorsport.
+										passionate about functionality and making things work. Prior
+										to entering the development space, I spent four years as a
+										professional racing driver, race team manager, and marketing
+										manager. I am excited to join a software development team in
+										which I can apply both my technical coding skills, as well
+										as the valuable teamwork and leadership experience I gained
+										during my previous career in motorsport.
 									</p>
 									<p className="px-4">
 										I built this portfolio site to showcase some of the awesome
