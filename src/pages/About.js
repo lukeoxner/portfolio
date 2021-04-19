1242d66ccd5b83759c39eb753b14b559d58e4208
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-04-19 09:17:46 -0600

    update resume link

--- src/pages/About.js
+++ src/pages/About.js
@@ -77,7 +77,7 @@ function About() {
 														className="resume-icon"
 													/>{" "}
 													<a
-														href="https://drive.google.com/file/d/1BPRGoBfl0n9ZJPIEeaKOjN02E_YucxL0/view?usp=sharing"
+														href="https://docs.google.com/document/d/1BzpjrR31BmYqwlmBy31EG9QS2x3gVzZsEzbYFwzrqwo/edit?usp=sharing"
 														target="blank"
 													>
 														My Resume
